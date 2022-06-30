38ef496f34b13db6a39d53dc1a44a59be76d6651
Fernweh99 <daniele.dimento@gmail.com>
2022-06-30 16:29:44 +0200

    finish bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -6,18 +6,11 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora
 di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
-# MILESTONE 1:
-Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
-
-#MILESTONE 2
-Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS.
-Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
-
-#MILESTONE 3
-In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
-
-#MILESTONE 4
-Al click sulla cella, stampiamo il numero della cella cliccata in console, poi coloriamo la cella d'azzurro!
+# BONUS
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 */
 
 //recupero la griglia dove inserire le celle
@@ -26,17 +19,42 @@ const grid = document.getElementById('grid');
 //recupero il bottone per generare le celle
 const playButton = document.getElementById('play');
 
+//recupero la select
+const selectChoice = document.getElementById('modality');
+
 //creo una funzione per creare le celle
 const getCreatecell = (content) => {
   const cell = document.createElement('div');
-  cell.className = 'cell';
+  cell.className = `cell`;
+  cell.classList.add(classMod)
   cell.innerText = content; 
   return cell;
 }
 
+let numberOfcells = 0;
+let classMod = '';
+
 //al click del bottone creo tot celle con un ciclo for
 playButton.addEventListener("click", ()=> {
-  for (let i = 1; i <= 100; i++){
+  grid.innerText=" ";
+  console.clear();  
+
+  if (selectChoice.value === "e") {
+    numberOfcells = 100;
+    classMod = "easy";
+  }
+  
+  else if (selectChoice.value === "m") {
+    numberOfcells = 81;
+    classMod = "medium";
+  }
+  
+  else if (selectChoice.value === "h") {
+    numberOfcells = 49;
+    classMod = "hard";
+  }
+
+  for (let i = 1; i <= numberOfcells; i++){
     const cell = getCreatecell(i);
     grid.appendChild(cell);
 
